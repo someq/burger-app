55640060752d4ceff733eb9993172a2c8ad7386d
Farid <thesomeq@gmail.com>
2019-02-25 19:56:45 +0600

    Подключен реакт-роутер в App.js и настроено отображение двух страниц: Burger и Checkout по разным путям.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <BurgerBuilder/>
+            <div className={'App'}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/" exact component={BurgerBuilder}/>
+                    </Switch>
+                </BrowserRouter>
             </div>
         );
     }
